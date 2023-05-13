b9938259f680cfce912526dba72054de3f22b1dc
Gourab Porey <gourabporey@users.noreply.github.com>
2023-05-13 13:01:35 +0530

    Added functionality to combine matched pair, solution is checking n^2

--- src/match-pair.js
+++ src/match-pair.js
@@ -1,5 +1,15 @@
 const matchPair = function (list1, list2) {
-  return 1;
+  const list3 = [];
+
+  for (const text1 of list1) {
+    for (const text2 of list2) {
+      if (text1.slice(text1.length - 4) === text2.slice(0, 4)) {
+        list3.push(text1 + text2.slice(4));
+      }
+    }
+  }
+
+  return list3;
 }
 
 exports.matchPair = matchPair;
--- test/match-pair-test.js
+++ test/match-pair-test.js
@@ -3,7 +3,11 @@ const { strictEqual, deepStrictEqual } = require("assert");
 const { matchPair } = require("../src/match-pair");
 
 describe("match-pair", () => {
-  it("Should give one", () => {
-    strictEqual(matchPair(), 1);
-  })
+  it("Should give the combined text if only one pair is present", () => {
+    deepStrictEqual(matchPair(["gourab"], ["urab56"]), ["gourab56"]);
+  });
+
+  it("Should give the combined texts for more than one element present", () => {
+    deepStrictEqual(matchPair(["gourab", "567milan"], ["ilan34", "urab56"]), ["gourab56", "567milan34"]);
+  });
 })
