e96ecd292f0812ed66d7051fe6a963e3e680873b
Gourab Porey <gourabporey@users.noreply.github.com>
2023-05-13 14:48:15 +0530

    Refactored by reducing one extra loop inside `matchPair` function

--- src/match-pair.js
+++ src/match-pair.js
@@ -1,19 +1,13 @@
 const matchPair = function (list1, list2) {
-  const list3 = [];
-
-  const lookup1 = {};
+  const lookup = {};
   for (const text of list1) {
-    lookup1[text.slice(text.length - 4)] = text;
+    lookup[text.slice(text.length - 4)] = text;
   }
 
-  const lookup2 = {};
-  for (const text of list2) {
-    lookup2[text.slice(0, 4)] = text;
-  }
-
-  for (const key in lookup1) {
-    list3.push(lookup1[key] + lookup2[key].slice(4));
-  }
+  const list3 = list2.map((text) => {
+    const precidingChars = text.slice(0, 4);
+    return lookup[precidingChars] + text.slice(4);
+  });
 
   return list3;
 }
